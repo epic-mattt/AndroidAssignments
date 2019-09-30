d056f0738afe42cb6235c51fd83b0a09fac7c1ad
epic-matt <buch6620@gmail.com>
2019-09-29 22:41:47 -0400

    a commit

--- app/src/main/java/com/example/acer/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/acer/androidassignments/LoginActivity.java
@@ -1,13 +1,63 @@
 package com.example.acer.androidassignments;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.content.Context;
+import android.widget.EditText;
+
+import SharedPreferences;
 
 public class LoginActivity extends Activity {
+
+    String preferences_file = "androidassignments_preferences_file";
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        editText = (EditText) findViewById(R.id.username_edittext);
+        Button button = findViewById(R.id.login_button);
+        SharedPreferences sharedPref = getSharedPreferences("email", Context.MODE_PRIVATE);
+        Button myButton = (Button)findViewById(R.id.login_button);
+        String email = sharedPref.getString("defaultEmail", "email@default.com;");
+        editText.setText(email);
+
+        button.setOnClickListener((x) -> {
+            SharedPreferences pref = getSharedPreferences("email",Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("email", editText.getText().toString());
+        });
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
     }
 }
--- app/src/main/java/com/example/acer/androidassignments/StartActivity.java
+++ app/src/main/java/com/example/acer/androidassignments/StartActivity.java
@@ -2,12 +2,42 @@ package com.example.acer.androidassignments;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
     }
 }
